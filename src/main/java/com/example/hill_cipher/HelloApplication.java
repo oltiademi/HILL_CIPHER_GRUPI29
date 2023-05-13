696ff6bc7af13e9d116fda7ae244d4ba2513d8ce
myhedinv <myhedin.vuciterna@student.uni-pr.edu>
2023-05-13 23:27:29 +0200

    Implemented key functions

--- src/main/java/com/example/hill_cipher/HelloApplication.java
+++ src/main/java/com/example/hill_cipher/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hillCipher.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1075, 600);
         stage.setTitle("Hill Cipher Encryption!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/hill_cipher/hillCipherController.java
+++ src/main/java/com/example/hill_cipher/hillCipherController.java
@@ -5,10 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,39 +23,63 @@ public class hillCipherController implements Initializable {
 
     @FXML
     private TextField keyTxt;
-
-    @FXML
-    private ComboBox output_ComboBox;
-
-
-    @FXML
-    void Decrypt(ActionEvent event) {
+    int getMatrixSize() {
+        String getSize = (String) inpuTxt_ComboBox.getValue();
+        int stringSize = Integer.parseInt(getSize);
+        return stringSize;
 
     }
-
-    @FXML
-    void EncryptText(ActionEvent event) {
-
+    double[][] getKeyMatrix(String key, double[][] keyMatrix) {
+        int size = getMatrixSize();
+        int k = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                keyMatrix[i][j] = (key.charAt(k)) % 65;
+                k++;
+            }
+        }
+        return keyMatrix;
     }
-
-    @FXML
-    void randomizeKey(ActionEvent event) {
-
+    Boolean checkKey(String key) {
+        if(key.length() == 0) {
+            return false;
+        }
+        try {
+            double[][] keyMatrix = new double[getMatrixSize()][getMatrixSize()];
+            double iMD = findInverseMod26(determinant(getKeyMatrix(key, keyMatrix)));
+            return true;
+        }catch(IllegalArgumentException e) {
+            key = "";
+            return false;
+        }
     }
-
+    String randomKey() {
+        final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String key = "";
+        int size = getMatrixSize() * getMatrixSize();
+        while(key.length() < size) {
+            char extra = upper.charAt((int) ((Math.random() * 25) + 0));
+            key += extra;
+        }
+        return key;
+    }
+    @FXML
+    void generateKey() {
+        String key = "";
+        if(getMatrixSize() <= 7) {
+            while (!checkKey(key)) {
+                key = randomKey();
+            }
+        } else {
+            key = randomKey();
+        }
+        keyTxt.setText(key);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
         // Set the items of the inputTxt_ComboBox ComboBox
-        ObservableList<String> inputSizes = FXCollections.observableArrayList("2x2", "3x3", "4x4");
+        ObservableList<String> inputSizes = FXCollections.observableArrayList("1", "2", "3", "4");
         inpuTxt_ComboBox.setItems(inputSizes);
-        inpuTxt_ComboBox.setValue("2x2");
-
-        // Set the items of the output_ComboBox ComboBox
-        ObservableList<String> outputSizes = FXCollections.observableArrayList("2x2", "3x3", "4x4");
-        output_ComboBox.setItems(outputSizes);
-        output_ComboBox.setValue("2x2");
+        inpuTxt_ComboBox.setValue("1");
     }
-
-
 }
